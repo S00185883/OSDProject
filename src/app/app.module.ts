f7e083206bf3802cbda4b73773f21b382aaf41cb
Tara McDonagh <47326811+TaraMcDonagh@users.noreply.github.com>
2021-03-17 01:09:21 +0000

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularTiltModule } from 'angular-tilt';
 import { APP_BASE_HREF } from '@angular/common';
-
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeCardComponent } from './recipe-card/recipe-card.component';
 import { ListRecipesComponent } from './list-recipes/list-recipes.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { NgAuthService } from "./services/ng-auth.service";
 import { TruncatePipe } from './trunc';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -29,8 +32,13 @@ import { TruncatePipe } from './trunc';
     FormsModule,
     AngularTiltModule,
     HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule, 
   ],
-  providers: [{ provide: APP_BASE_HREF, useValue: '/home' }],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/home' },NgAuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
