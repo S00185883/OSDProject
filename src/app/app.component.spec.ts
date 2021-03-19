0e84f11ab6be784a7bacad90231418e4590e0186
Tara McDonagh <47326811+TaraMcDonagh@users.noreply.github.com>
2021-03-19 20:37:32 +0000

    getting the search bar back

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fire-make-better'`, () => {
+  it(`should have as title 'angular-book-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fire-make-better');
+    expect(app.title).toEqual('angular-book-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('fire-make-better app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-book-app app is running!');
   });
 });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyAVdvjQ41PDZzI0pqbanS8rD3cn2Q0TeV0",
-  authDomain: "project-d49bb.firebaseapp.com",
-  databaseURL: "https://project-d49bb.firebaseio.com",
-  projectId: "project-d49bb",
-  storageBucket: "project-d49bb.appspot.com",
-  messagingSenderId: "62726120902",
-  appId: "1:62726120902:web:ae0bdb169d62c1882cfec7",
-  measurementId: "G-9SHMQ7B72P"
-  }
+  production: false
 };
 
 /*
