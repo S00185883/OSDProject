78c95dcae25027cac37e94f1b9fa44863778a31b
Tara McDonagh <47326811+TaraMcDonagh@users.noreply.github.com>
2021-03-24 20:35:07 +0000

    profile with saved recipes

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
 import { NgAuthService } from "src/app/services/ng-auth.service";
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import {RecipeService} from 'src/app/services/recipe.service'
+import { AngularFirestore } from '@angular/fire/firestore'; 
+import { Recipe } from 'src/app/recipe';
+import { APP_BASE_HREF } from '@angular/common';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  base_href: string;
 
-  constructor(public ngAuthService: NgAuthService) { }
-
-  ngOnInit(): void {
+  constructor(public ngAuthService: NgAuthService,private firestore: AngularFirestore,private recipeservice:RecipeService,@Inject(APP_BASE_HREF) private baseHref: string){}
+  ngOnInit() {
+    this.getSavedRecipes();
   }
 
+  recipes;
+
+  getSavedRecipes = () =>
+    this.recipeservice
+      .getSavedRecipes()
+      .subscribe(res => (this.recipes = res));
+
+  deleteRecipe = data => this.recipeservice.deleteRecipe(data);
+  onDelete() {
+    let data = this.recipes;
+    this.recipeservice.deleteRecipe(data);
+  
+       
+  }
 }
